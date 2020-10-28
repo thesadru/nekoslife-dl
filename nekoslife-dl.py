45b40377b05798c366a51fc3e25534f411a21101
thesadru <dan0.suman@gmail.com>
2020-10-28 20:24:54 +0100

    Made NekosLife more readable, made a command line downloader.

--- nekoslife-dl.py
+++ nekoslife-dl.py
@@ -2,67 +2,80 @@ from nekoslife import NekosLife
 import argparse
 import time
 
+"""
+need to define:
+
+__init__
+save_folder: str = 'images'
+download_threads: int = 1
+url_file: str = None
+sort_url_file: bool = False
+
+wait_until_finished 
+timeout: int = 60*60
+"""
 parser = argparse.ArgumentParser('nekoslife-dl')
 
 parser.add_argument('type',choices=NekosLife.ENDPOINT_TYPES)
 parser.add_argument('format',choices=NekosLife.ENDPOINT_FORMATS)
 parser.add_argument('category')
-parser.add_argument('save_folder',default='images')
-
-parser.add_argument('--timeout',
-    type=int,
-    default=300,
-    help='After what time the program stop downloading.')
-parser.add_argument('--repeat',
-    type=int,
-    default=100,
-    help='How many times to repeat getting images.')
-
-parser.add_argument('-u','--url-folder',
-    default=None,
-    help='Saves data to a url folder instead of downloading.')
-parser.add_argument('-r','--read-url-folder',
+parser.add_argument('-q','--quiet',
     action='store_true',
-    help='Downloads from a url folder instead of writing to it.')
+    help="Does not show the progress bar."
+)
 
-parser.add_argument('--threads',
+download = parser.add_argument_group('download')
+download.add_argument('-a','--amount',
+    type=int,
+    default=20,
+    help="Amount of images to try and download."
+)
+download.add_argument('-u','--unique',
+    type=int,
+    default=0,
+    help="Amount of unique images to download,\
+          when <= 0, the program downloads images and doesn't care about duplicates."
+)
+download.add_argument('--timeout',
+    type=int,
+    default=60*60,
+    help="How many seconds to wait until release lock, if download is taking too long."
+)
+
+paths = parser.add_argument_group('paths')
+paths.add_argument('-f','--folder',
+    default='images',
+    help="Where to save all the images."
+)
+paths.add_argument('-F','--url-file',
+    default=None,
+    help="Uses a file to store all gotten urls. Can later read from it to save time."
+)
+
+utility = parser.add_argument_group('utility')
+utility.add_argument('--threads',
+    type=int,
     default=1,
-    help='Amount of threads used for download')
+    help="How many threads will be used to download images."
+)
+utility.add_argument('--sort-url-file',
+    action='store_true',
+    help="Sorts urls in the url file. No functionality."
+)
 
 
 args = parser.parse_args()
-print(args)
 
-nekoslife = NekosLife(args.save_folder, download_threads=args.threads)
+nekoslife = NekosLife(
+    args.folder, download_threads = args.threads, 
+    progress_bar = not args.quiet,
+    url_file = args.url_file, sort_url_file = args.sort_url_file)
+nekoslife.raise_for_category(args.type,args.format,args.category)
 
-endpoints = nekoslife.get_endpoints()
-categories = endpoints[args.type][args.format]
-if args.category not in categories:
-    raise Exception('category must be in [%s]' % ','.join(categories))
-
-
-start = time.time()
-
-if args.url_folder is not None and args.read_url_folder:
-    with open(args.url_folder,'r') as file:
-        for url in file.readlines():
-            url = url[:-1]
-            urldata = nekoslife.url_to_imagedata(url)
-            if nekoslife.should_enqueue(*urldata):
-                nekoslife.dlqueue.put(urldata)
-    
-else:
-    urls = set()
-    for i in range(args.repeat):
-        urls.update(nekoslife.get_images(args.type,args.format,args.category))
-
-    if args.url_folder:
-        with open(args.url_folder,'w') as file:
-            file.write('\n'.join(urls))
-    else:
-        nekoslife.add_to_dlqueue(urls)
-
-print(urls)
-print('GOT ALL URLS, NOW ONLY DOWNLOADING')
-while time.time()-start < args.timeout:
-    pass
+nekoslife.get_multiple_images(
+    args.type,args.format,args.category,
+    amount = args.amount,
+    add_to_dlqueue = True,
+    use_url_file = args.url_file is not None,
+    unique = args.unique)
+nekoslife.wait_until_finished(args.timeout)
--- nekoslife.py
+++ nekoslife.py
@@ -11,15 +11,16 @@ from queue import Queue
 
 import requests
 
+
 class NekosLife:
     """
     Downloads data from nekos.life.
     Uses https://api.nekos.dev/api/v3/.
     Since there is no offical API for v3, I made one myself.
-    
+
     It gets images using the API, then adds them into `dlqueue`.
     Download workers will then download it.
-    
+
     For the simplest behaviour you can use this
     ```
     from nekoslife import NekosLife
@@ -28,38 +29,38 @@ class NekosLife:
     nekoslife.wait_until_finished()
     ```
     """
-    
+
     # constants
-    MAX_IMAGE_COUNT = 20 # 20 is currently max
+    MAX_IMAGE_COUNT = 20  # 20 is currently max
     # urls
     URL = "https://api.nekos.dev/api/v3/"
     IMAGES_URL = URL+'images/'
     GET_URL = IMAGES_URL+"{t}/{f}/{c}/?count={a}"
     CDN_URL = "https://cdn.nekos.life/v3/{t}/{f}/{c}/{i}"
-    
+
     PROGRESS_BAR = "\r[*] {d} / {u} images [{t} / {e}]"+' '*6
     start_dl_time = time.time()
     estimated_time = 0
-    
+
     # settings
-    CHUNK_SIZE = 0x100000 # 1MibB
-    ENDPOINT_TYPES = 'sfw','nsfw'
-    ENDPOINT_FORMATS = 'img','gif'
+    CHUNK_SIZE = 0x100000  # 1MibB
+    ENDPOINT_TYPES = 'sfw', 'nsfw'
+    ENDPOINT_FORMATS = 'img', 'gif'
     SHOW_PROGRESS_BAR = False
     SORT_URL_FILE = False
-    
+
     # variables
     dlqueue = Queue()
     endpoints = {}
-    
+
     save_folder = None
     url_file = None
-    
+
     def __init__(self,
-            save_folder: str='images', download_threads: int=1, 
-            progress_bar: bool=False,
-            url_file: str=None, sort_url_file: bool=False,
-            generate_endpoints=False):
+        save_folder: str = 'images', download_threads: int = 1,
+        progress_bar: bool = False,
+        url_file: str = None, sort_url_file: bool = False,
+        generate_endpoints: bool = False):
         """
         `save_folder` is the path to downloaded images.
         `download_threads` initializes the set amount of `download workers`.
@@ -72,147 +73,22 @@ class NekosLife:
         """
         self.save_folder = save_folder
         self.url_file = url_file
-        
+
         if not os.path.isdir(self.save_folder):
             os.makedirs(self.save_folder)
-        
+
         self.SHOW_PROGRESS_BAR = progress_bar
         self.SORT_URL_FILE = sort_url_file
-        
+
         if generate_endpoints:
             self.get_endpoints()
-        
+
         self._start_download_workers(download_threads)
-            
-    def generate_image_url(self,imgtype,imgformat,imgcategory,amount=None):
-        """
-        Generates url to get images.
-        """
-        if amount is None:
-            amount = self.MAX_IMAGE_COUNT
-        
-        if not (2 <= amount <= self.MAX_IMAGE_COUNT):
-            raise ValueError(f'Amount must be 2 < amount <= {self.MAX_IMAGE_COUNT}.')
-        
-        url = self.GET_URL.format(t=imgtype,f=imgformat,c=imgcategory,a=amount)
-        if amount > 2:
-            return url
-        else:
-            return url[:8]
 
-    def get_endpoints(self,force=False) -> dict:
-        """
-        Gets all endpoints by requesting bad urls and getting corrected.
-        If endpoints have been already once generated, returns the old endpoints.
-        To overcome this use `force`.
-        """
-        if self.endpoints and not force:
-            return self.endpoints
-        
-        endpoints = {}
-        for i in self.ENDPOINT_TYPES:
-            endpoints[i] = {}
-            for j in self.ENDPOINT_FORMATS:
-                url = self.IMAGES_URL+i+'/'+j+'/'
-                data = requests.get(url).json()['data']
-                endpoints[i][j] = data['response']['categories']
-        
-        self.endpoints = endpoints
-        
-        return endpoints
-    
-    @staticmethod
-    def _to_minutes(seconds):
-        """
-        Progress bar tool that converts seconds into a `?min ?s` format
-        """
-        minutes, seconds= map(int,divmod(seconds,60))
-        if minutes == 0:
-            return f'{seconds}s'
-        else:
-            return f'{minutes}min {seconds}s'
-    
-    def print_progress_bar(self):
-        """
-        Prints the progress bar.
-        When `SHOW_PROGRESS_BAR` is False, doesn't print.
-        """
-        if not self.SHOW_PROGRESS_BAR:
-            return False
-        
-        downloaded_urls = len(os.listdir(self.save_folder))
-        planned_urls = self.dlqueue.qsize()
-        urls = downloaded_urls + planned_urls
-        current_time = round(time.time()-self.start_dl_time, 2)
-        estimated_time = round(self.estimated_time*urls, 2)
-        
-        print(
-            self.PROGRESS_BAR.format(
-                d=downloaded_urls,u=urls,
-                t=self._to_minutes(current_time),e=self._to_minutes(estimated_time)),
-            end=''
-        )
-        return True
-    
-    def update_estimated_time(self,time):
-        """
-        Updates estimated time of seconds per image.
-        Should only be used internally.
-        """
-        if self.estimated_time is None:
-            self.estimated_time = time
-        else:
-            urls = len(os.listdir(self.save_folder))
-            self.estimated_time = (urls*self.estimated_time+time)/(urls+1)
+    # =========================================================================
+    # get images
 
-    @staticmethod
-    def url_index(url):
-        """
-        Gets the number in the image filename.
-        Useful for sorting or guessing missing images.
-        """
-        filename = os.path.split(url)[1]
-        number = ''
-        for i in filename:
-            if i.isdigit():
-                number += i
-        return int(number)
-    
-    def get_urls_file(self):
-        """
-        Gets all urls from `url_file`. Urls must be seperated by `\\n`.
-        """
-        if self.url_file is None or not os.path.isfile(self.url_file):
-            return []
-        
-        urls = []
-        with open(self.url_file,'r') as file:
-            for url in file.readlines():
-                if len(url) > 1:
-                    url = url.strip('\n')
-                    urls.append(url)
-        return urls
-    
-    def set_urls_file(self,urls):
-        """
-        Sets the content of the `url_file` into urls.
-        """
-        if self.url_file is None:
-            return []
-        
-        with open(self.url_file,'w') as file:
-            if self.SORT_URL_FILE:
-                urls = sorted(urls,key=self.url_index)
-            file.write('\n'.join(urls))
-        return urls
-
-    def add_urls_file(self,urls):
-        """
-        Adds urls into `url_file`.
-        """
-        return self.set_urls_file(set(urls).union(self.get_urls_file()))
-
-    def get_images(self,imgtype: str, imgformat: str, imgcategory: str, amount: int = None):
+    def get_images(self, imgtype: str, imgformat: str, imgcategory: str, amount: int = None):
         """
         Gets images from nekos.life, look at `get_endpoints()` to see all endpoints.
         Size of list is `amount`, `MAX_IMAGE_COUNT` by default.
@@ -222,34 +98,22 @@ class NekosLife:
             amount = self.MAX_IMAGE_COUNT
         elif amount == 0:
             return 0
-        
-        url = self.generate_image_url(imgtype,imgformat,imgcategory)
+
+        url = self.generate_image_url(imgtype, imgformat, imgcategory)
         r = requests.get(url)
         if r.status_code != 200:
             return r.status_code
-        
+
         data = r.json()['data']
         if not data['status']['success']:
-            raise ValueError('You must supply a proper category, check endpoints.')
+            raise ValueError(
+                'You must supply a proper category, check endpoints.')
         urls = data['response']['urls']
-        
+
         return urls
-    
-    @staticmethod
-    def _number_split(n,s):
-        """
-        Splits n into a sorted array a of integers x, 
-        where x <= s and the lenght of a is the smallest possible.
-        """
-        if n == 0:
-            return ()
-        i = 0
-        for i in range(s,n,s):
-            yield s
-        yield n-i
-    
+
     def get_multiple_images(self,
-            imgtype: str, imgformat: str, imgcategory: str, amount: int, 
+            imgtype: str, imgformat: str, imgcategory: str, amount: int,
             add_to_dlqueue: bool = False,
             use_url_file=False,
             unique: int = 0):
@@ -264,36 +128,41 @@ class NekosLife:
             urls = self.get_urls_file()
             if add_to_dlqueue:
                 self.add_to_dlqueue(urls)
-        
+        else:
+            urls = []
+
         if unique <= 0:
-            for i in self._number_split(amount,self.MAX_IMAGE_COUNT):
-                new_urls = self.get_images(imgtype,imgformat,imgcategory,i)
+            for i in self._number_split(amount, self.MAX_IMAGE_COUNT):
+                new_urls = self.get_images(imgtype, imgformat, imgcategory, i)
                 urls.extend(new_urls)
-                
+
                 if add_to_dlqueue:
                     self.add_to_dlqueue(new_urls)
                 self.print_progress_bar()
-        
+
         else:
             urls = set(urls)
-            for i in self._number_split(amount,self.MAX_IMAGE_COUNT):
-                new_urls = self.get_images(imgtype,imgformat,imgcategory,i)
+            for i in self._number_split(amount, self.MAX_IMAGE_COUNT):
+                new_urls = self.get_images(imgtype, imgformat, imgcategory, i)
                 new_urls = set(new_urls).difference(urls)
                 urls.update(new_urls)
-                
+
                 if add_to_dlqueue:
                     self.add_to_dlqueue(new_urls)
                 self.print_progress_bar()
-                
+
                 if len(urls) >= unique:
                     break
-        
+
         if use_url_file:
             self.add_urls_file(set(urls))
-        
+
         return urls
-    
-    def add_to_dlqueue(self,urls):
+
+    # =========================================================================
+    # download
+
+    def add_to_dlqueue(self, urls):
         """
         Adds urls to `dlqueue`.
         Uses methods `should_enqueue(url_to_imagedata(url))` to check url validity.
@@ -303,17 +172,17 @@ class NekosLife:
             urldata = self.url_to_imagedata(url)
             if self.should_enqueue(*urldata):
                 self.dlqueue.put(urldata)
-    
-    def url_to_imagedata(self,url):
+
+    def url_to_imagedata(self, url):
         """
         Converts a url to `(url, filename, path)`.
         """
         filename = os.path.split(url)[-1]
-        path = os.path.join(self.save_folder,filename)
-        
-        return url,path,filename
-    
-    def should_enqueue(self,url,path,filename):
+        path = os.path.join(self.save_folder, filename)
+
+        return url, path, filename
+
+    def should_enqueue(self, url, path, filename):
         """
         Checks wheter a url should be enqueued.
         Valid if file does not exist.
@@ -321,16 +190,8 @@ class NekosLife:
         if os.path.exists(path):
             return False
         return True
-        
-    def empty_dlqueue(self):
-        """
-        Marks all tasks unstarted tasks as done.
-        """
-        while not self.dlqueue.empty():
-            self.dlqueue.get()
-            self.dlqueue.task_done()
-    
-    def wait_until_finished(self,timeout=60*60):
+
+    def wait_until_finished(self, timeout=60*60):
         """
         Waits until all downloads are finished.
         Returns True when everything was downloaded.
@@ -340,11 +201,13 @@ class NekosLife:
         while self.dlqueue.unfinished_tasks:
             if time.time() - start > timeout:
                 return False
-        
-        return True
-                
 
-    def download_url(self,url,path,dlpath=None):
+        return True
+
+    # =========================================================================
+    # download workers
+
+    def download_url(self, url, path, dlpath=None):
         """
         Downloads a file and saves it to path.
         Dlpath will be the file path while still downloading.
@@ -354,11 +217,11 @@ class NekosLife:
         r = requests.get(url)
         if dlpath is None:
             dlpath = path
-        with open(dlpath,'wb') as file:
+        with open(dlpath, 'wb') as file:
             for chunk in r.iter_content(self.CHUNK_SIZE):
                 file.write(chunk)
-        os.rename(dlpath,path)
-        
+        os.rename(dlpath, path)
+
         return time.time() - start
 
     def download_worker(self):
@@ -367,29 +230,209 @@ class NekosLife:
         Updates the estimated time and prints progress bar every download.
         """
         while True:
-            url,path,filename = self.dlqueue.get()
-            
+            url, path, filename = self.dlqueue.get()
+
             try:
                 dlpath = path+'.000'
-                dl_time = self.download_url(url,path,dlpath)
+                dl_time = self.download_url(url, path, dlpath)
                 self.update_estimated_time(dl_time)
-            except (Exception,KeyboardInterrupt):
+            except (Exception, KeyboardInterrupt):
                 if os.path.isfile(dlpath):
                     os.remove(dlpath)
-            
+
             self.print_progress_bar()
             self.dlqueue.task_done()
-    
-    def _start_download_workers(self,amount):
+
+    def _start_download_workers(self, amount):
         """
         Starts a set amount of download workers.
         Remember to run this function only once.
         """
         self._download_workers = []
         for i in range(amount):
-            t = threading.Thread(target=self.download_worker,name=f'nldlworker_{i}',daemon=True)
+            t = threading.Thread(target=self.download_worker,
+                                 name=f'nldlworker_{i}', daemon=True)
             t.start()
             self._download_workers.append(t)
+
+    # =========================================================================
+    # url file
+
+    def get_urls_file(self):
+        """
+        Gets all urls from `url_file`. Urls must be seperated by `\\n`.
+        """
+        if self.url_file is None or not os.path.isfile(self.url_file):
+            return []
+
+        urls = []
+        with open(self.url_file, 'r') as file:
+            for url in file.readlines():
+                if len(url) > 1:
+                    url = url.strip('\n')
+                    urls.append(url)
+        return urls
+
+    def set_urls_file(self, urls):
+        """
+        Sets the content of the `url_file` into urls.
+        """
+        if self.url_file is None:
+            return []
+
+        with open(self.url_file, 'w') as file:
+            if self.SORT_URL_FILE:
+                urls = sorted(urls, key=self.url_index)
+            file.write('\n'.join(urls))
+        return urls
+
+    def add_urls_file(self, urls):
+        """
+        Adds urls into `url_file`.
+        """
+        return self.set_urls_file(set(urls).union(self.get_urls_file()))
+
+    # =========================================================================
+    # progress bar
+
+    def print_progress_bar(self):
+        """
+        Prints the progress bar.
+        When `SHOW_PROGRESS_BAR` is False, doesn't print.
+        """
+        if not self.SHOW_PROGRESS_BAR:
+            return False
+
+        downloaded_urls = len(os.listdir(self.save_folder))
+        planned_urls = self.dlqueue.qsize()
+        urls = downloaded_urls + planned_urls
+        current_time = round(time.time()-self.start_dl_time, 2)
+        estimated_time = round(self.estimated_time*urls, 2)
+
+        print(
+            self.PROGRESS_BAR.format(
+                d=downloaded_urls, u=urls,
+                t=self._to_minutes(current_time), e=self._to_minutes(estimated_time)),
+            end=''
+        )
+        return True
+
+    def update_estimated_time(self, time):
+        """
+        Updates estimated time of seconds per image.
+        Should only be used internally.
+        """
+        if self.estimated_time is None:
+            self.estimated_time = time
+        else:
+            urls = len(os.listdir(self.save_folder))
+            self.estimated_time = (urls*self.estimated_time+time)/(urls+1)
+
+    # =========================================================================
+    # static methods
+
+    @staticmethod
+    def _to_minutes(seconds):
+        """
+        Progress bar tool that converts seconds into a `?min ?s` format
+        """
+        minutes, seconds = map(int, divmod(seconds, 60))
+        if minutes == 0:
+            return f'{seconds}s'
+        else:
+            return f'{minutes}min {seconds}s'
+
+    @staticmethod
+    def url_index(url):
+        """
+        Gets the number in the image filename.
+        Useful for sorting or guessing missing images.
+        """
+        filename = os.path.split(url)[1]
+        number = ''
+        for i in filename:
+            if i.isdigit():
+                number += i
+        return int(number)
+
+    @staticmethod
+    def _number_split(n, s):
+        """
+        Splits n into a sorted array a of integers x, 
+        where x <= s and the lenght of a is the smallest possible.
+        """
+        if n == 0:
+            return ()
+        i = 0
+        for i in range(s, n, s):
+            yield s
+        yield n-i
+
+    # =========================================================================
+    # utility
+
+    def generate_image_url(self, imgtype, imgformat, imgcategory, amount=None):
+        """
+        Generates url to get images.
+        """
+        if amount is None:
+            amount = self.MAX_IMAGE_COUNT
+
+        if not (2 <= amount <= self.MAX_IMAGE_COUNT):
+            raise ValueError(
+                f'Amount must be 2 < amount <= {self.MAX_IMAGE_COUNT}.')
+
+        url = self.GET_URL.format(
+            t=imgtype, f=imgformat, c=imgcategory, a=amount)
+        if amount > 2:
+            return url
+        else:
+            return url[:8]
+
+    def get_endpoints(self, force=False) -> dict:
+        """
+        Gets all endpoints by requesting bad urls and getting corrected.
+        If endpoints have been already once generated, returns the old endpoints.
+        To overcome this use `force`.
+        """
+        if self.endpoints and not force:
+            return self.endpoints
+
+        endpoints = {}
+        for i in self.ENDPOINT_TYPES:
+            endpoints[i] = {}
+            for j in self.ENDPOINT_FORMATS:
+                url = self.IMAGES_URL+i+'/'+j+'/'
+                data = requests.get(url).json()['data']
+                endpoints[i][j] = data['response']['categories']
+
+        self.endpoints = endpoints
+
+        return endpoints
+
+    def raise_for_category(self, imgtype, imgformat, imgcategory):
+        """
+        Raises ValueError if category is invalid.
+        """
+        if imgtype not in self.ENDPOINT_TYPES:
+            raise ValueError('type must be in [%s]'
+                             % ','.join(self.ENDPOINT_TYPES))
+        if imgformat not in self.ENDPOINT_FORMATS:
+            raise ValueError('format must be in [%s]'
+                             % ','.join(self.ENDPOINT_FORMATS))
+
+        endpoints = self.get_endpoints()[imgtype][imgformat]
+        if imgcategory not in endpoints:
+            raise ValueError(f'category for {imgtype}/{imgformat} must be in [%s]'
+                             % ','.join(endpoints))
+
+    def empty_dlqueue(self):
+        """
+        Marks all tasks unstarted tasks as done.
+        """
+        while not self.dlqueue.empty():
+            self.dlqueue.get()
+            self.dlqueue.task_done()
 
 
 if __name__ == "__main__":
